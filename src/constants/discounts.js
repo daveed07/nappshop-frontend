56e9203ee36b53868ec90f63b2b2da990f341edb
David Zuñiga <davidz0703@gmail.com>
2022-07-07 09:22:02 -0500

    Added functionality for discount codes

---
+++ src/constants/discounts.js
@@ -0,0 +1,17 @@
+exports.discounts = [
+  {
+    name: '10%OFF',
+    description: '10% off on all products',
+    discount: 10,
+  },
+  {
+    name: '20%OFF',
+    description: '20% off on all products',
+    discount: 20,
+  },
+  {
+    name: 'BLACKFRIDAY',
+    description: 'Black Friday discount',
+    discount: 70,
+  }
+]
