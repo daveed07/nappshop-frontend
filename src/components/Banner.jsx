d55ffe48102f74edd4ad005de6ce084ce7b0be92
David Zuñiga <davidz0703@gmail.com>
2022-09-18 16:40:08 -0500

    Fixing loading

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { assets } from "@constants/assets";
 import StyledBanner from "@styles/styledBanner";
 import StyledLoading from "@styles/styledLoading";
+import Skeleton from "react-loading-skeleton";
 import colors from "@constants/colors";
 
 const Banner = () => {
@@ -17,13 +18,7 @@ const Banner = () => {
   return (
     <StyledBanner>
       {loading ? (
-        <StyledLoading
-          className="react-loader"
-          type="bubbles"
-          color={colors.main}
-          height={50}
-          width={50}
-        />
+        <Skeleton duration={2} height={450} />
       ) : (
         <img src={assets.banner} alt="banner" />
       )}
