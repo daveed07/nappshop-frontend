9c9a5f5852aae77e24d0cfade0d3ccdd021a570c
David Zuñiga <davidz0703@gmail.com>
2022-09-18 12:59:44 -0500

    Fixing loading

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from "react";
 import { assets } from "@constants/assets";
 import StyledBanner from "@styles/styledBanner";
 import StyledLoading from "@styles/styledLoading";
+import colors from "@constants/colors";
 
 const Banner = () => {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     const loadData = async () => {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+      await new Promise((resolve) => setTimeout(resolve, 5000));
       setLoading((loading) => !loading);
     };
     loadData();
@@ -19,7 +20,7 @@ const Banner = () => {
       <StyledLoading
         className="react-loader"
         type="spin"
-        color="#000"
+        color={colors.main}
         height={50}
         width={50}
       />
