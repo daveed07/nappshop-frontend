129cd98a8200a84e35b29d226db7f52989042b14
David Zuñiga <davidz0703@gmail.com>
2022-10-31 19:43:59 -0500

    Adding payment status to order

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -29,6 +29,11 @@ const Order = ({ order, loading }) => {
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
 
+  const paymentStatus = order.payment_status
+    .split("_")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+
   const [loadOrder, setLoadOrder] = useState(false);
 
   useEffect(() => {
