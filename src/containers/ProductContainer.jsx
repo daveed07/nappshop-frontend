ca8a4f57739ca1837b1bc59c0b6a2ceb7de9f018
David Zuñiga <davidz0703@gmail.com>
2022-11-30 18:23:34 -0500

    Fixed issue on order confirmation page

--- src/containers/ProductContainer.jsx
+++ src/containers/ProductContainer.jsx
@@ -7,11 +7,12 @@ import StyledProductContainer from "@styles/styledProductContainer";
 import { env } from "@constants/env";
 
 const ProductContainer = ({ filter, loading }) => {
+  console.log(filter)
   const { brand } = useParams();
   const queryUrl = `${env.API}/products?${
     brand
       ? `filterByBrand=${brand}`
-      : filter.brand && filter.brand !== "all"
+      : filter && filter.brand !== "all"
       ? `filterByBrand=${filter.brand}`
       : ""
   }${
